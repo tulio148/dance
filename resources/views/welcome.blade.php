1d54ff7358d4c5e67c0bd152c1c243429ccd58e1
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-08-31 05:57:49 +0800

    removed laravel/cashier

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,5 @@
 <x-guest-layout>
-    <div class="front-page-content absolute inset-0 w-screen">
-        <video autoplay loop class="w-full h-auto">
-            <source src="{{ asset('storage\video_mc.mp4') }}" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-    </div>
 
-    <div class="mt-[10000px]">test</div>
 
 
 </x-guest-layout>
