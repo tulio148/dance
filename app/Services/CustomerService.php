838702b7894d4b9de77ba50a9034c8219b715cae
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-09-13 07:15:40 +0800

    created customerService class for customer logic

---
+++ app/Services/CustomerService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+use Square\SquareClient;
+use App\Models\Customer;
+use Square\Models\CreateCustomerRequest;
+
+class CustomerService
+{
+    public function createCustomer($user)
+    {
+        $client = app(SquareClient::class);
+        $body = new CreateCustomerRequest();
+        $body->setIdempotencyKey(uniqid());
+        $body->setGivenName($user->name);
+        $body->setEmailAddress($user->email);
+
+        $api_response = $client->getCustomersApi()->createCustomer($body);
+        
+        if ($api_response->isSuccess()) {
+            $result = $api_response->getResult();
+            $customer = Customer::updateOrCreate(
+                ["email" => $result->getCustomer()->getEmailAddress()],
+                [
+                    'id' => $result->getCustomer()->getId(),
+                    'name' => $result->getCustomer()->getGivenName(),
+                    'email' => $result->getCustomer()->getEmailAddress()
+                ]
+            );
+            $user->customer_id = $result->getCustomer()->getId();
+            $user->save();
+
+        } else {
+            $errors = $api_response->getErrors();
+            dd($errors);
+        }
+    }
+
+    public function retrieveCustomer($user)
+    {
+        $client = app(SquareClient::class);
+        $api_response = $client->getCustomersApi()->retrieveCustomer($user->customer_id);
+        if ($api_response->isSuccess()) {
+            $result = $api_response->getResult();
+            return $result;
+        } else {
+            $errors = $api_response->getErrors();
+            dd($errors);
+        }
+    }
+}
